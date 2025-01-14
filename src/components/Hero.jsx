4dec4a374ab99029efda79418809776e65d83ddd
Skund <Skund.kr@gmail.com>
2025-01-14 19:35:54 +0530

    commit-2 removed loader

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -75,15 +75,7 @@ const Hero = () => {
       );
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
-      {isLoading && (
-        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-black">
-          <div className="three-body">
-            <div className="three-body__dot"></div>
-            <div className="three-body__dot"></div>
-            <div className="three-body__dot"></div>
-          </div>
-        </div>
-      )}
+      
 
         <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
             <div>
