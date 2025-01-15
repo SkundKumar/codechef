ebcaa022d4ad335b9366c9de3e75a4ead0ad442a
Skund <Skund.kr@gmail.com>
2025-01-15 09:25:13 +0530

    images insertion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Contact from "./components/Contact"
 export default function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero/>
       <About/>
       <Features/>
+      <Contact/>
+      <Footer/>
     </main>
   )
 }
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useRef } from 'react';
 const BentoCard = ({src,title,description,isComingSoon})=>{
     const isVideo = src.endsWith('.mp4') || src.endsWith('.webm');
     return(
@@ -28,10 +29,46 @@ const BentoCard = ({src,title,description,isComingSoon})=>{
         </div>
     )
 }
+const BentoTilt = ({children,className=" "})=>{
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (event.clientX - left) / width;
+    const relativeY = (event.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.9) * 5;
+    const tiltY = (relativeX - 0.9) * -5;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
+
+  return (
+    <div
+      ref={itemRef}
+      className={className}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  )
+}
 const Features = () => {
   return (
     <section className="bg-black pb-52">
-        <div className="container mx-auto px-3 md:px-10">
+        <div className=" container mx-auto px-3 md:px-10">
             <div className="px-5 py-32">
                 <p className="font-circular-web text-lg text-blue-50">
                     MEET OUR TEAM
@@ -45,38 +82,91 @@ const Features = () => {
             
             </p>
             </div>
-            <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
+            <BentoTilt className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
                 <BentoCard
                 src="videos/feature-1.mp4"
                 title={<>Pres<b>id</b>ent</>}
                 description={<b>Ishanvi Verma</b>}
                 isComingSoon
                 />
-            </div>
-            <div className='grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-9 '>
-            <div className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
+            </BentoTilt>
+            <div className='grid h-[90vh] w-full grid-cols-2 grid-rows-2 mb-9 gap-9 '>
+            <BentoTilt className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
                 <BentoCard
                 src="videos/feature-2.png"
                 title={<>M<b>ul</b>timedi<b>a</b> & Desi<b>gn</b><br/> <b>h</b>ead</>}
                 description={<><b>Skund Kumar</b></>}
                 />
-           </div>
-           <div className='bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0'>
+           </BentoTilt>
+           <BentoTilt className='bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0'>
                 <BentoCard
                 src={"videos/vp.jpg"}
                 title={<>Vice <br/><b>Pre</b>sident</>}
                 description={<><b>Manan Malik</b></>}
                 />
-           </div>
-           <div className='bento-tilt_1 me-14 md:col-span-1 md:me-0'>
+           </BentoTilt>
+           <BentoTilt className='bento-tilt_1 me-14  md:col-span-1 md:me-0'>
                 <BentoCard
-                src={"videos/sec.jpg"}
+                src={"videos/adya.png"}
                 title={<>Gen <br/><b>sec</b>ratary</>}
                 description={<><b>Adya Rastogi</b></>}
                 />
-           </div>
-
+           </BentoTilt>
+           
             </div>
+            <div className='grid h-[90vh] w-full grid-cols-2 grid-rows-2 gap-9 mb-9'>
+              
+            <BentoTilt className='bento-tilt_1 md:col-span-1 md:me-0'>
+                <BentoCard
+                src={"videos/karan.jpg"}
+                title={<>Man<b>A</b>gement <br/> Head</>}
+                description={<><b>Karan Tyagi</b></>}
+                />
+           </BentoTilt>
+           <BentoTilt className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2'>
+                <BentoCard
+                src="videos/featue-2.png"
+                title={<><span className="text-white">Te<b>c</b>h <b>h</b>ead</span></>}
+                description={<><b>Chrag</b></>}
+                />
+           </BentoTilt>
+           <BentoTilt className='bento-tilt_1 ms-20 md:col-span-1 md:ms-0'>
+                <BentoCard
+                src={"videos/social.jpg"}
+                title={<>Soc<b>Al</b> Media <br/> Head</>}
+                description={<><b>Shreya</b></>}
+                />
+           </BentoTilt>
+           
+            
+            </div>
+            <div className='grid h-[50vh] w-full grid-cols-2 grid-rows-1 gap-9 '>
+           <BentoTilt className='bento-tilt_1 mr-24 md:col-span-1 md:me-0'>
+                <BentoCard
+                src={"videos/naman.jpg"}
+                title={<>Fin<b>A</b>nce<br/> Head</>}
+                description={<><b>Naman Garg</b></>}
+                />
+           </BentoTilt>
+           <BentoTilt className='bento-tilt_1 ms-24 md:col-span-1 md:ms-0'>
+                <BentoCard
+                src={"videos/socia.jpg"}
+                title={<><span className="text-white">PR<b> & o</b>utreach<br/> Head</span></>}
+                description={<><b>raunak</b></>}
+                />
+           </BentoTilt>
+           
+           </div>
+           <BentoTilt className='border-hsla relative mb-7 mt-9 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
+                
+                <BentoCard
+                
+                src="videos/hero-1.mp4"
+                title={<><span className="text-white">The<b> T</b>eam</span></>}
+              
+                />
+                
+            </BentoTilt>
         </div>
     </section>
   )
