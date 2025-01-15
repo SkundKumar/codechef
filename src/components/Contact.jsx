7869a0b610f0fff6fd118a7e59b8e02a51e88425
Skund <Skund.kr@gmail.com>
2025-01-15 10:15:16 +0530

    final changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import AnimatedTitle from "./AnimatedTitle";
+import Button from "./Button";
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div id="contact" className="my-20 min-h-96 w-screen  px-10">
+      
+      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+          <div className="flex flex-col items-center text-center">
+        
+          <p className="mb-10 font-general text-[12px] uppercase">
+            Join The Event
+          </p>
+            <div className="container overflow-hidden ">
 
-export default Contact
+            
+         <h1 className="special-font !md:text-[10rem] w-full font-zentry !text-8xl !font-black !leading-[1]">"Fill the<b> Fo</b>rm to<br /> register now."</h1>
+            
+         </div>
+
+          <Button title="contact us" containerClass="mt-10 cursor-pointer" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
